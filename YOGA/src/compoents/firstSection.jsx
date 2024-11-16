2ada369170d113b9cdcb3b4074b4a134800ff5e2
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-11-16 12:30:14 -0700

    added a more meaningful line

--- YOGA/src/compoents/firstSection.jsx
+++ YOGA/src/compoents/firstSection.jsx
@@ -7,8 +7,8 @@ function FirstSection() {
       <div className="first-content">
       <p className='p1'>Mind-Body-Soul Balance</p>
         <h2>Achieve balance in mind, body, and soul.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur. Semper malesuada ut ullamcorper sed tortor curabitur.</p>
-      </div>
+        <p>Embrace a journey that balances your physical strength, mental clarity, and inner peace through the practice of yoga. Our holistic approach to yoga not only rejuvenate the body but also nourishes the mind and uplifts the spirit, creating a balanced foundation for a healthier, more centered life.</p>
+        </div>
       <div className="first-image">
         <img src="\images\trianer_5-removebg-preview.png" alt="Yoga Pose" />
       </div>
